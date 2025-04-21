bfd6be09d3ac61b7507d0a0e1e2d6fae8a6784f2
Haseeb MernStack <seebpubg@gmail.com>
2025-04-21 16:12:53 +0500

    first commit

---
+++ Syntax and Basics/basic.js
@@ -0,0 +1,14 @@
+// Syntax and Basics
+ 
+
+//! 1. Variables.
+
+//  var, let, const.
+
+//! 2. Data Types.
+
+//  string, number, boolean, null, undefined, symbol, bigint 
+
+//! 3. Operators.
+
+//  Arithmetic, Assignment, Comparison, Logical, Bitwise, Ternary.
